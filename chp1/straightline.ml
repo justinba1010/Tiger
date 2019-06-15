38470143530b9f19bbf072f22f75ba38ba9ba158
Justin Baum <justinba1010@gmail.com>
2019-06-15 15:17:13 -0400

    Working

--- chp1/straightline.ml
+++ chp1/straightline.ml
@@ -59,23 +59,28 @@ and maxArgsPrint = List.length
   List.fold_left (fun acc x -> acc + (maxArgs x)) 0 statements
 *)
 
+let rec assoclookup key = function
+    | [] -> 0
+    | (k, v)::_t when k = key -> v
+    | (_k, v)::t -> assoclookup key t
+
 
 let interp (statement: stm) =
   let rec interpStm: (stm -> pair list -> pair list) = fun (statement: stm) -> fun (ids: pair list) ->
     match statement with
-    | AssignStm(id, exp) -> [(id, interpExp exp)] @ ids
+    | AssignStm(id, exp) -> [(id, interpExp exp ids)] @ ids
     | CompoundStm(stm1, stm2) -> interpStm stm2 ids @ interpStm stm1 ids
-    | PrintStm(stms) -> []
-  and interpExp: (exp -> int) = fun (expression: exp) ->
+    | PrintStm(stms) -> (List.iter (fun exp -> (print_int (interpExp exp ids))) stms; ids) 
+  and interpExp: (exp -> pair list -> int) = fun (expression: exp) -> fun (pairs: pair list) ->
     match expression with
     | NumExp(num) -> num
-    | OpExp(num1, Plus, num2) -> interpExp num1 + interpExp num2
-    | OpExp(num1, Subt, num2) -> interpExp num1 - interpExp num2
-    | OpExp(num1, Mult, num2) -> interpExp num1 * interpExp num2
-    | OpExp(num1, Div, num2) -> interpExp num1 / interpExp num2
+    | OpExp(num1, Plus, num2) -> interpExp num1 pairs + interpExp num2 pairs
+    | OpExp(num1, Subt, num2) -> interpExp num1 pairs - interpExp num2 pairs
+    | OpExp(num1, Mult, num2) -> interpExp num1 pairs * interpExp num2 pairs
+    | OpExp(num1, Div, num2) -> interpExp num1 pairs / interpExp num2 pairs
     | EseqExp(_stm, _exp) -> 0 (* Unimplemented currently *)
-    | IdExp(_id) -> 0 (* Unimplemented currently *)
-  in interpStm(statement)
+    | IdExp(id) -> assoclookup id pairs
+  in interpStm statement []
 let print_2 (pairs : pair list) =
   List.iter
   (fun a ->
@@ -87,4 +92,3 @@ let print_2 (pairs : pair list) =
   ) pairs
 let pairs = interp (prog)
 let _ = print_2 (pairs)
-let _ = print_2 [("v", 123); ("b", 789)]
