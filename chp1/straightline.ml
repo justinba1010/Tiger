790f3034114f081820232c665350c33284977192
Justin Baum <justinba1010@gmail.com>
2019-06-15 05:28:21 -0400

    Tidy it up :)

--- chp1/straightline.ml
+++ chp1/straightline.ml
@@ -9,20 +9,23 @@ type id = string
 
 type pair = id * int
 
-type binop = Plus 
-  | Subt 
-  | Mult 
+type binop =
+  | Plus
+  | Subt
+  | Mult
   | Div
 
-type stm = CompoundStm of stm * stm
+type stm =
+  |CompoundStm of stm * stm
   | AssignStm of id * exp
   | PrintStm of exp list
-and exp = IdExp of id
+and exp =
+  | IdExp of id
   | NumExp of int
   | OpExp of exp * binop * exp
   | EseqExp of stm * exp
-  
-let prog = 
+
+let prog =
   CompoundStm(
     AssignStm("a",
       OpExp(NumExp 5, Plus, NumExp 3)),
@@ -57,31 +60,31 @@ and maxArgsPrint = List.length
 *)
 
 
-let interp (statement: stm) = 
-    let rec interpStm: (stm -> pair list -> pair list) = fun (statement: stm) -> fun (ids: pair list) -> 
-        match statement with
-        | AssignStm(id, exp) -> [(id, interpExp exp)] @ ids
-        | CompoundStm(stm1, stm2) -> interpStm stm2 ids @ interpStm stm1 ids
-        | PrintStm(stms) -> []
-    and interpExp: (exp -> int)  = fun (expression: exp) -> 
-        match expression with
-        | NumExp(num) -> num
-        | OpExp(num1, Plus, num2) -> interpExp num1 + interpExp num2
-        | OpExp(num1, Subt, num2) -> interpExp num1 - interpExp num2
-        | OpExp(num1, Mult, num2) -> interpExp num1 * interpExp num2
-        | OpExp(num1, Div, num2) -> interpExp num1 / interpExp num2
-        | EseqExp(_stm, _exp) -> 0 (* Unimplemented currently *)
-        | IdExp(_id) -> 0 (* Unimplemented currently *)
-    in interpStm(statement)
-let print_2 (pairs : pair list) = 
-    List.iter
-    (fun a -> 
-        let (id, valu) = a in
-        print_string id; 
-        print_string "-";
-        print_int valu;
-        print_string ("\n")
-    ) pairs
+let interp (statement: stm) =
+  let rec interpStm: (stm -> pair list -> pair list) = fun (statement: stm) -> fun (ids: pair list) ->
+    match statement with
+    | AssignStm(id, exp) -> [(id, interpExp exp)] @ ids
+    | CompoundStm(stm1, stm2) -> interpStm stm2 ids @ interpStm stm1 ids
+    | PrintStm(stms) -> []
+  and interpExp: (exp -> int) = fun (expression: exp) ->
+    match expression with
+    | NumExp(num) -> num
+    | OpExp(num1, Plus, num2) -> interpExp num1 + interpExp num2
+    | OpExp(num1, Subt, num2) -> interpExp num1 - interpExp num2
+    | OpExp(num1, Mult, num2) -> interpExp num1 * interpExp num2
+    | OpExp(num1, Div, num2) -> interpExp num1 / interpExp num2
+    | EseqExp(_stm, _exp) -> 0 (* Unimplemented currently *)
+    | IdExp(_id) -> 0 (* Unimplemented currently *)
+  in interpStm(statement)
+let print_2 (pairs : pair list) =
+  List.iter
+  (fun a ->
+    let (id, valu) = a in
+    print_string id;
+    print_string "-";
+    print_int valu;
+    print_string ("\n")
+  ) pairs
 let pairs = interp (prog)
 let _ = print_2 (pairs)
 let _ = print_2 [("v", 123); ("b", 789)]
