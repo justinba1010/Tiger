6ad3edfc3a00e5767008b9f531f36cec0fdb4af3
Justin Baum <justinba1010@gmail.com>
2019-06-15 03:43:26 -0400

    No longer working :(

--- chp1/straightline.ml
+++ chp1/straightline.ml
@@ -7,6 +7,8 @@
 
 type id = string
 
+type pair = id * int
+
 type binop = Plus 
   | Subt 
   | Mult 
@@ -53,3 +55,30 @@ and maxArgsPrint = List.length
 (*maxArgsPrint: stm list -> int = fun (statements: stm list) ->
   List.fold_left (fun acc x -> acc + (maxArgs x)) 0 statements
 *)
+
+
+let interp (statement: stm) = 
+    let rec interpStm: (stm -> pair list -> pair list) = fun (statement: stm) -> fun (ids: pair list) -> 
+        match statement with
+        | AssignStm(id, exp) -> [(id, interpExp exp)] @ ids
+        | CompoundStm(stm1, stm2) -> interpStm stm2 ids @ interpStm stm1 ids
+        | PrintStm(stms) -> []
+    and interpExp: (exp -> int)  = fun (expression: exp) -> 
+        match expression with
+        | NumExp(num) -> num
+        | OpExp(num1, Plus, num2) -> interpExp num1 + interpExp num2
+        | OpExp(num1, Subt, num2) -> interpExp num1 - interpExp num2
+        | OpExp(num1, Mult, num2) -> interpExp num1 * interpExp num2
+        | OpExp(num1, Div, num2) -> interpExp num1 / interpExp num2
+        | EseqExp(_stm, _exp) -> 0 (* Unimplemented currently *)
+        | IdExp(_id) -> 0 (* Unimplemented currently *)
+    in interpStm(statement)
+let print_2 = List.iter(
+    (fun ((id, value)) ->
+        print_string id;
+        print_string "-";
+        print_int value;
+        print_string "\n"
+    )
+)
+print_2 (interp prog)
